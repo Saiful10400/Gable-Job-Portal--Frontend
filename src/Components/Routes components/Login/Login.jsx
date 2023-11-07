d720fcedaa1bea221f42e612051342330e8e6e03
saiful10400 <saifulislam10400i5@gmail.com>
2023-11-07 10:24:10 +0600

    registration route is done.

--- src/Components/Routes components/Login/Login.jsx
+++ src/Components/Routes components/Login/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
             <img src={loginsti} className="h-full object-contain " alt="" />
           </div>
 
-          <div className="w-[60%] h-full py-5 pr-10">
+          <div className="w-[60%] h-full py-5 px-28">
             <h1 className="text-end text-lg mb-5 font-bold">
               New user? <Link to={"/register"} className="text-[#38A745] hover:underline">Register</Link>
             </h1>
@@ -33,16 +33,16 @@ const Login = () => {
               <div className="relative">
               <input
                 placeholder="Your E-mail"
-                className="   w-full rounded-md pl-32 text-2xl font-bold  outline outline-black focus:outline-4  focus:outline-green-500 h-[82px]"
+                className="   w-full rounded-md pl-32 text-xl font-bold  outline outline-black focus:outline-4  focus:outline-green-500 h-[50px]"
                 type="text"
-              /> <FaUser className="absolute top-7  left-10 text-3xl text-[#38A745]"></FaUser>
+              /> <FaUser className="absolute top-2  left-10 text-3xl text-[#38A745]"></FaUser>
               </div>
               <div className="relative">
               <input
                 placeholder="Your Password"
-                className="  w-full rounded-md pl-32 text-2xl font-bold outline outline-black focus:outline-4  focus:outline-green-500  h-[82px]"
+                className="  w-full rounded-md pl-32 text-xl font-bold outline outline-black focus:outline-4  focus:outline-green-500  h-[50px]"
                 type="text"
-              /><BiSolidLock className="absolute top-7  left-10 text-3xl text-[#38A745]"></BiSolidLock>
+              /><BiSolidLock className="absolute top-2  left-10 text-3xl text-[#38A745]"></BiSolidLock>
               </div>
               <div className="flex gap-9">
                 <button className="btn w-[40%] text-lg h-20 rounded-full bg-[#38A745] text-white">
